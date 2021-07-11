3e966df54ad97688d83f863bf06a4f64257bf4c6
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-07-11 00:26:21 -0300

    Criação do projeto e integração com o Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from './components/Button'
+
 function App() {
-  return (
-    <h1>Hello World!</h1>
+  return(
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
   );
 }
 
