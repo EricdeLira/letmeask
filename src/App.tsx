67a7bc907a2ae7416c018c01eac41f81dc1cba23
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-07-11 22:27:47 -0300

    Criação de rotas, autenticação, contextos e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+  
   return(
-    <NewRoom />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/room/new" component={NewRoom}/>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
+//import { useAuth } from '../hooks/useAuth'
+
 import illustrationImg from '../assets/images/illustration.svg' //webpack
 import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg' 
 
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
 
-
 export function NewRoom(){
+    //const { user } = useAuth();
+
     return(
         <div id="page-auth">
             <aside>
@@ -29,9 +32,8 @@ export function NewRoom(){
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala já existente?<a href="#">Clique aqui</a>
-                    </p>
-                    
+                        Quer entrar em uma sala já existente?<Link to="/">Clique aqui</Link>
+                    </p>    
                 </div>
             </main>
         </div>
