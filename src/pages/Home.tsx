5f0836f55e4df334d6b77bcdbc4979a6cc537947
Eric de Lira Candido <ericdelira2001@gmail.com>
2021-07-16 16:22:34 -0300

    Excluir perguntas e encerrar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,11 @@ export function Home(){
             return;
         }
 
+        if(roomRef.val().endedAt){
+            alert('Room is already closed.');
+            return;
+        }
+
         history.push(`rooms/${roomCode}`);
     }
 
